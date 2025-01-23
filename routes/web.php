5a88a64d8c9f6e9f6923bbc65b5e43817d6e3841
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-23 16:41:01 +0100

    new laravel Course

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function (){
+    return view('about');
+});
+
+Route::view('/azer', 'about');
+
+
+Route::controller(CarController::class)->group(function (){
+    Route::get("/car",'index');
 });
