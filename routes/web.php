68a39b23253f1aa279fe38ee329b120ecf99ae0c
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-23 23:35:04 +0100

    new laravel Course

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MathController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function (){
-    return view('about');
-});
-
-Route::view('/azer', 'about');
-
-
-Route::get('/sum/{a}/{b}',[MathController::class,'sum'])
-    ->whereNumber(['a','b']);
-Route::get('/subtract/{a}/{b}',[MathController::class,'subtract'])
-    ->whereNumber(['a','b']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
